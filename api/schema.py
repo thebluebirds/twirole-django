33f7a7182eb21c0bd247d9890fcd5ad70bde59d2
Kenneth Worden <kenny.worden@gmail.com>
2019-02-25 01:07:12 -0500

    Update model for multiclassifier, add stacked bar charts

--- api/schema.py
+++ api/schema.py
@@ -12,33 +12,48 @@ from graphene import (
     Field,
 )
 
-from random import random
-
-from .twirole import twirole_classify
+from .twirole import classify
 
 
+# Coerces a raw dictionary to the GraphQL objects so it can be returned by the API
+def coerce_to_classification(raw_classification):
+    res = Classification()
+    res.male = raw_classification['Male']
+    res.female = raw_classification['Female']
+    res.brand = raw_classification['Brand']
+    return res
+
+
+# A classification for some subclass
 class Classification(ObjectType):
     male = Float()
     female = Float()
     brand = Float()
 
 
+# The entire output as returned by TwiRole
+class ModelOutput(ObjectType):
+    bf = Field(Classification)
+    af = Field(Classification)
+    cnn = Field(Classification)
+    hybrid = Field(Classification)
+
+
+# We only have one type of query, the "classify" query
 class Query(ObjectType):
-    classify = Field(Classification, { 'handle': String(required=True) })
+    classify = Field(ModelOutput, { 'handle': String(required=True) })
 
     def resolve_classify(self, info, handle):
-        result = Classification()
+        # Uses TwiRole to classify; returns a raw JSON dictionary
+        twirole_results = classify(handle)
 
-        # Uses TwiRole to classify
-        twirole_results = twirole_classify(handle)
+        output = ModelOutput()
+        output.bf = coerce_to_classification(twirole_results['BF'])
+        output.af = coerce_to_classification(twirole_results['AF'])
+        output.cnn = coerce_to_classification(twirole_results['CNN'])
+        output.hybrid = coerce_to_classification(twirole_results['Hybrid'])
 
-        result.male = twirole_results['male']
-        result.female = twirole_results['female']
-        result.brand = twirole_results['brand']
-
-        print('classified', twirole_results)
-
-        return result
+        return output
 
 
 schema = Schema(query=Query)
--- api/twirole.py
+++ api/twirole.py
@@ -2,34 +2,19 @@
 Runs the Twirole model.
 """
 
-from re import search
-from subprocess import (
-    run,
-    PIPE,
-)
+from subprocess import run
+from json import loads
+from re import match
 
 
-def twirole_classify(twitter_handle):
-    cmd = f'\'source activate py27; ./classify.sh {twitter_handle}\''
-    proc = run(['sh', '-c', cmd],
-               cwd='/Users/kkworden/Code/school/Twirole',
+def classify(twitter_handle):
+    proc = run(['./classify.sh', twitter_handle],
+               cwd='/home/cs4624s19_role/TwiRole',
                capture_output=True)
 
-    matches = search(r'\[(.*)\]', proc.stdout.decode('utf-8'))
-    male, female, brand = 0, 0, 0
+    matches = match(r'(.+?)({.*})', proc.stdout.decode('utf-8'))
 
-    for raw_output in matches.group(1).split(', '):
-        if raw_output.startswith('Male'):
-            male = float(raw_output.replace('Male: ', '')[:-1])
-        elif raw_output.startswith('Female'):
-            female = float(raw_output.replace('Female: ', '')[:-1])
-        elif raw_output.startswith('Brand'):
-            brand = float(raw_output.replace('Brand: ', '')[:-1])
-        else:
-            raise ValueError('Unable to parse TwiRole classifer output.')
-
-    return {
-        'male': male,
-        'female': female,
-        'brand': brand,
-    }
+    if matches:
+        return loads(matches.group(2))
+    else:
+        raise ValueError('Unable to parse TwiRole classifer output.')
